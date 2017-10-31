230785d239c1dd2d96f77a5b0e00abdab33eea66
co-deion <cdphicks.0@gmail.com>
2017-10-31 16:03:38 -0500

    added cookie-parser

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,23 @@
 import { join } from 'path';
 import * as express from 'express';
 import { json } from 'body-parser';
+import * as cookieParser from 'cookie-parser'
 import apiRouter from './api';
 import stateRouting from './middleware/routing.mw';
-import configurePassport from './config/passport';
+import configurePassport from './config/passport'; 
 
 const clientPath = join(__dirname, '../client');
 const app = express();
 
 
 app.use(express.static(clientPath));
+app.use(cookieParser());
 app.use(json());
-// app.use('/api', apiRouter);
-// app.get('*', stateRouting);
+
+configurePassport(app);
+
+app.use('/api', apiRouter);
+app.get('*', stateRouting);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on Port: 3000');
