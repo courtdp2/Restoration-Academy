f6cf0f95f0f678c72a09b7f0c1a08088e6205edf
Rebecca Jurgens <rjvideography@Rebeccas-MacBook-Pro.local>
2017-11-01 16:16:40 -0500

    programs/controllers/factories

--- client/controllers.js
+++ client/controllers.js
@@ -1,1 +1,27 @@
 angular.module('RA.controllers', [])
+
+.controller('ContactController', ['$scope', 'ContactForm', function ($scope, ContactForm) {
+    $scope.send = function () {
+        let contact = new ContactForm({
+            from: $scope.email,
+            message: $scope.message
+        });
+
+        contact.$save(function () {
+            alert('Thank you for you message. We will get back with you shortly')
+        }, function (err) {
+            console.log(err);
+        });
+    }
+}])
+
+.controller('ProgramController', ['$scope', 'Programs', function ($scope, Programs) {
+    let navSelector = '#toc';
+    let $myNav = $(navSelector);
+    Toc.init($myNav);
+    $('body').scrollspy({
+        target: navSelector
+    });
+}]);
+
+    
