47f1895ca802dedf4597ee4a2fe326c3652629d0
ellenhartporter <31662182+ellenhartporter@users.noreply.github.com>
2017-11-02 15:41:10 -0500

    merging

---
+++ client/factories.js
@@ -0,0 +1,15 @@
+angular.module('RA.factories', [])
+.factory('About', ['$resource', function($resource) {
+    return $resource('api/about/:id', { id: '@id'}, {
+    })
+}])
+.factory('ContactForm', ['$resource', function($resource) {
+    return $resource('/api/contact/:id', { id: '@id'});
+
+// }])
+// .factory('Purchase', ['$resource', function($resource) {
+//     return $resource('api/purchases/:id', {id: '@id'});
+// }])
+// .factory('Payment', ['$resource', function($resource) {
+//     return $resource('api/payment/:id', {id: '@id'})
+}]);
